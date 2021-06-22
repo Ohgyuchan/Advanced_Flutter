367c0cdb544fc9fa8c9a862c16ca2b195e00f66a
Ohgyuchan <gyu3637@gmail.com>
2021-06-22 16:34:43 +0900

    Add resizeToAvoidBottomInset -> home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,6 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: DefaultTabController(
         length: 4,
         child: Scaffold(
