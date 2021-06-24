9aebe39946c8b7c8b29eac81a0c87af8edf44333
Ohgyuchan <gyu3637@gmail.com>
2021-06-24 22:50:25 +0900

    conflict 해결

--- lib/Management/FeedController.dart
+++ lib/Management/FeedController.dart
@@ -1,35 +1,3 @@
-import 'package:advanced_flutter/repositories/feed_repository.dart';
-import 'package:advanced_flutter/screens/sign_in_screen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:get/get.dart';
-import 'package:advanced_flutter/models/feed_model.dart';
-import 'AuthController.dart';
-
-class FeedController extends GetxController{
-
-  static FeedController instance = Get.find();
-  RxList<FeedModel> feedList = RxList<FeedModel>([]);
-  String collection = "feedLists";
-
-  // ignore: invalid_use_of_protected_member
-  
-  List<FeedModel> get feeds => feedList.value;
-
-  //
-
-
-  @override
-  void onReady() {
-    super.onReady();
-    String uid = currentUser.uid;
-    feedList
-        .bindStream(FeedRepository().feedStream(uid)); //stream coming from firebase
-  }
-
-
-
-}
 // import 'package:advanced_flutter/repositories/feed_repository.dart';
 // import 'package:advanced_flutter/screens/sign_in_screen.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
@@ -39,16 +7,21 @@ class FeedController extends GetxController{
 // import 'AuthController.dart';
 //
 // class FeedController extends GetxController{
-//   RxList<FeedModel> feedList = RxList<FeedModel>();
+//
+//   static FeedController instance = Get.find();
+//   RxList<FeedModel> feedList = RxList<FeedModel>([]);
+//   String collection = "feedLists";
 //
 //   // ignore: invalid_use_of_protected_member
+//
 //   List<FeedModel> get feeds => feedList.value;
 //
-//   //static FeedController instance = Get.find();
+//   //
 //
 //
 //   @override
-//   void onInit() {
+//   void onReady() {
+//     super.onReady();
 //     String uid = currentUser.uid;
 //     feedList
 //         .bindStream(FeedRepository().feedStream(uid)); //stream coming from firebase
@@ -57,3 +30,30 @@ class FeedController extends GetxController{
 //
 //
 // }
+// // import 'package:advanced_flutter/repositories/feed_repository.dart';
+// // import 'package:advanced_flutter/screens/sign_in_screen.dart';
+// // import 'package:cloud_firestore/cloud_firestore.dart';
+// // import 'package:firebase_core/firebase_core.dart';
+// // import 'package:get/get.dart';
+// // import 'package:advanced_flutter/models/feed_model.dart';
+// // import 'AuthController.dart';
+// //
+// // class FeedController extends GetxController{
+// //   RxList<FeedModel> feedList = RxList<FeedModel>();
+// //
+// //   // ignore: invalid_use_of_protected_member
+// //   List<FeedModel> get feeds => feedList.value;
+// //
+// //   //static FeedController instance = Get.find();
+// //
+// //
+// //   @override
+// //   void onInit() {
+// //     String uid = currentUser.uid;
+// //     feedList
+// //         .bindStream(FeedRepository().feedStream(uid)); //stream coming from firebase
+// //   }
+// //
+// //
+// //
+// // }
--- lib/repositories/feed_repository.dart
+++ lib/repositories/feed_repository.dart
@@ -1,74 +1,5 @@
-import 'package:advanced_flutter/models/feed_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class FeedRepository {
-  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
-
-  @override
-  Future<List<FeedModel>> getFeedList() async{
-    List<FeedModel> feeds = [];
-
-    try {
-      var feedCollection =
-          await _firebaseFirestore.collection('post').get();
-      for (var feed in feedCollection.docs){
-        feeds.add(FeedModel.fromDs(feed));
-      }
-    } catch (e){
-      throw Exception('firebase error');
-    }
-    return feeds;
-
-  }
-
-  Stream<List<FeedModel>> feedStream(String uid) {
-    return FirebaseFirestore.instance
-        .collection("post")
-        .snapshots()
-        .map((QuerySnapshot query) {
-      List<FeedModel> retVal =  [];
-      query.docs.forEach((element) {
-        retVal.add(FeedModel.fromDs(element));
-      });
-      return retVal;
-    });
-  }
-
-  Future<void> addFeed(String content, String uid) async {
-    try {
-      await _firebaseFirestore
-          .collection("post")
-          .add({
-        'dateCreated': Timestamp.now(),
-        'content': content,
-        'done': false,
-      });
-    } catch (e) {
-      print(e);
-      rethrow;
-    }
-  }
-
-  Future<void> updateFeed(bool newValue, String uid, String todoId) async {
-    try {
-      _firebaseFirestore
-          .collection("users")
-          .doc(uid)
-          .collection("todos")
-          .doc(todoId)
-          .update({"done": newValue});
-    } catch (e) {
-      print(e);
-      rethrow;
-    }
-  }
-
-
-}
-
 // import 'package:advanced_flutter/models/feed_model.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/cupertino.dart';
 //
 // class FeedRepository {
 //   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
@@ -81,7 +12,7 @@ class FeedRepository {
 //       var feedCollection =
 //           await _firebaseFirestore.collection('post').get();
 //       for (var feed in feedCollection.docs){
-//         feeds.add(FeedModel.fromDocumentSnapshot(feed));
+//         feeds.add(FeedModel.fromDs(feed));
 //       }
 //     } catch (e){
 //       throw Exception('firebase error');
@@ -97,7 +28,7 @@ class FeedRepository {
 //         .map((QuerySnapshot query) {
 //       List<FeedModel> retVal =  [];
 //       query.docs.forEach((element) {
-//         retVal.add(FeedModel.fromDocumentSnapshot(element));
+//         retVal.add(FeedModel.fromDs(element));
 //       });
 //       return retVal;
 //     });
@@ -135,3 +66,72 @@ class FeedRepository {
 //
 // }
 //
+// // import 'package:advanced_flutter/models/feed_model.dart';
+// // import 'package:cloud_firestore/cloud_firestore.dart';
+// // import 'package:flutter/cupertino.dart';
+// //
+// // class FeedRepository {
+// //   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+// //
+// //   @override
+// //   Future<List<FeedModel>> getFeedList() async{
+// //     List<FeedModel> feeds = [];
+// //
+// //     try {
+// //       var feedCollection =
+// //           await _firebaseFirestore.collection('post').get();
+// //       for (var feed in feedCollection.docs){
+// //         feeds.add(FeedModel.fromDocumentSnapshot(feed));
+// //       }
+// //     } catch (e){
+// //       throw Exception('firebase error');
+// //     }
+// //     return feeds;
+// //
+// //   }
+// //
+// //   Stream<List<FeedModel>> feedStream(String uid) {
+// //     return FirebaseFirestore.instance
+// //         .collection("post")
+// //         .snapshots()
+// //         .map((QuerySnapshot query) {
+// //       List<FeedModel> retVal =  [];
+// //       query.docs.forEach((element) {
+// //         retVal.add(FeedModel.fromDocumentSnapshot(element));
+// //       });
+// //       return retVal;
+// //     });
+// //   }
+// //
+// //   Future<void> addFeed(String content, String uid) async {
+// //     try {
+// //       await _firebaseFirestore
+// //           .collection("post")
+// //           .add({
+// //         'dateCreated': Timestamp.now(),
+// //         'content': content,
+// //         'done': false,
+// //       });
+// //     } catch (e) {
+// //       print(e);
+// //       rethrow;
+// //     }
+// //   }
+// //
+// //   Future<void> updateFeed(bool newValue, String uid, String todoId) async {
+// //     try {
+// //       _firebaseFirestore
+// //           .collection("users")
+// //           .doc(uid)
+// //           .collection("todos")
+// //           .doc(todoId)
+// //           .update({"done": newValue});
+// //     } catch (e) {
+// //       print(e);
+// //       rethrow;
+// //     }
+// //   }
+// //
+// //
+// // }
+// //
